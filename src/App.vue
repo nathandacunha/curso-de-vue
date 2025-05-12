def83c44f5b8d66659d8329223bcd881f8a156db
nathandacunha <nathan_cunha@estudante.sesisenai.org.br>
2025-05-12 12:44:58 -0300

    Curso de Vuejs

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+  import { computed, computed} from 'vue'
+
+  const contador1 = ref(0)
+  const contador2 = ref(0)
+  const valorBooleano = ref(true)
+
+  function incrementar(){
+    eval(contador).value++
+  }
+
+  function decrementar(){
+    contador.value--
+  }
+
+</script>
+
+<template>
+  <div>
+    <button @></button>
+  </div>
+</template>
+
