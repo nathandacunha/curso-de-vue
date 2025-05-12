7e5be84ac766692ee48fd3aa635bb0f84e88fb24
nathandacunha <nathan_cunha@estudante.sesisenai.org.br>
2025-05-12 12:50:07 -0300

    começo da correção dos exercicios  reatividade

--- src/App.vue
+++ src/App.vue
@@ -10,14 +10,41 @@
   }
 
   function decrementar(){
-    contador.value--
+    if (contador.value > 0){
+      contador.value--;
+    }
+  }
+
+  function reiniciar(){
+    contador.value = 0;
+  }
+
+  function inverterTextoEmMaiusculo(texto){
+    return texto.split('').reverse().join('').toUpperCase()
   }
 
 </script>
 
 <template>
-  <div>
-    <button @></button>
+  <h1>
+    correção dos exercicios
+  </h1>
+
+  <div class="info">
+    <h2>
+      informações
+    </h2>
+    <p>Nome: {{  nome  }}</p>
+    <p>Idade: {{ idade }} anos</p>
+    <hr>
+    <p>
+      Para completar 50 anos faltam: {{ 50 - idade }} anos
+    </p>
+
+    <p>
+      O nome invertido em maisculo é: {{ inverterTextoEmMaiusculo(nome) }}
+    </p>
   </div>
+
 </template>
 
